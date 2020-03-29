36156e5566a1e33ab6af3f9c05203e19fdbbaf4f
Kornél <leitner.kornel@gmail.com>
2020-03-29 14:11:30 +0200

    Print out the FileWordAnalyzer class results

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         FilePartReader inputText = new FilePartReader();
-        inputText.setup("src/main/resources/sample.txt", 1, 102);
-        System.out.println(inputText.readLines());
-    }
+        inputText.setup("src/main/resources/sample.txt", 1, 100);
 
+        String substring = "s";
+
+        FileWordAnalyzer analyzer = new FileWordAnalyzer(inputText);
+        System.out.println("Alphabetically ordered words");
+        System.out.println(analyzer.getWordsOrderedAlphabetically());
+        System.out.println("\nWords what are containing the given substring (" + substring + ") - Case insensitive: ");
+        System.out.println(analyzer.getWordsContainingSubstring(substring));
+        System.out.println("\nPalindrome words");
+        System.out.println(analyzer.getStringWhichPalindromes());
+    }
 }
